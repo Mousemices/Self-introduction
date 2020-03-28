a1772f565395f509ff9a78daf49180238de644d4
Mousemices <9.1271613xia@gmail.com>
2020-03-28 15:22:57 +0100

    Title fade in animation completed

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+
+window.addEventListener('load', function() {
+    console.log('All assets are loaded');
+    const title = document.querySelector('.title');
+    title.classList.add('fade-in');
+});
